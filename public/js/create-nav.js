46bfdcaa9f9c2000d987c1cf1722529c579738b3
ducanhpn <phamngocducanh19@gmail.com>
2024-04-29 14:59:21 +0200

    Create Navigation

---
+++ public/js/create-nav.js
@@ -0,0 +1,43 @@
+const nav = document.getElementById("nav");
+const ul = document.createElement("ul");
+
+nav.appendChild(ul);
+
+const li = [];
+const menuItem = ['Home', 'Kategorie', 'Verkaufen', 'Unternehmen'];
+
+//add <li> to <ul>
+for(let i=0; i<4; i++){
+    li.push(document.createElement("li"));
+    li[i].textContent = menuItem[i];
+    li[i].classList.add(menuItem[i]);
+    ul.appendChild(li[i]);
+}
+
+const subItem = ['Philosophie', 'Karriere'];
+const subLiItem = [];
+const subUl = document.createElement(("ul"))
+subUl.hidden = true;
+
+li[li.length - 1].appendChild(subUl); // add <ul> to <li>Unternehmen</li>
+
+//add sub <li> to sub <ul>
+for(let i=0; i < subItem.length; i++){
+    subLiItem.push(document.createElement('li'));
+    subLiItem[i].textContent = subItem[i]
+    subLiItem[i].classList.add(subItem[i]);
+    subUl.appendChild(subLiItem[i]);
+}
+
+//handle routing
+li[1].addEventListener('click', function(){
+    window.location.href = '/article';
+})
+
+// config sub list
+li[li.length-1].addEventListener('mouseenter', function(){
+    subUl.hidden= false;
+})
+subUl.addEventListener('mouseout', function(){
+    subUl.hidden= true;
+})
---
+++ public/js/data.js
@@ -0,0 +1,90 @@
+var data = {
+    'produkte': [
+        { name: 'Ritterburg', preis: 59.99, kategorie: 1, anzahl: 3 },
+        { name: 'Gartenschlau 10m', preis: 6.50, kategorie: 2, anzahl: 5 },
+        { name: 'Robomaster' ,preis: 199.99, kategorie: 1, anzahl: 2 },
+        { name: 'Pool 250x400', preis: 250, kategorie: 2, anzahl: 8 },
+        { name: 'Rasenmähroboter', preis: 380.95, kategorie: 2, anzahl: 4 },
+        { name: 'Prinzessinnenschloss', preis: 59.99, kategorie: 1, anzahl: 5 }
+    ],
+    'kategorien': [
+        { id: 1, name: 'Spielzeug' },
+        { id: 2, name: 'Garten' }
+    ]
+};
+
+const getMaxPreis = (data) => {
+    let max = data['produkte'][0].preis;
+    for( const obj of data['produkte']){
+        if(obj.preis >= max) max = obj.preis;
+    }
+    return max;
+}
+
+const getMinPreis = (data) => {
+    let min = data['produkte'][0].preis;
+    for( const obj of data['produkte']){
+        if(obj.preis <= min) min = obj.preis;
+    }
+    return min;
+}
+
+const getPreisSum = (data) => {
+
+    return data['produkte'].map(element => element.preis).reduce((accumulaltor, currentValue) => accumulaltor + currentValue);
+}
+
+const getGesamtWert = (data) => {
+
+    return data['produkte'].map(element => element.preis * element.anzahl)
+        .reduce((accumulaltor, currentValue) => accumulaltor + currentValue);
+}
+
+const getAnzahlProdukteOfKategorie = (data, kategorieName = 'Spielzeug') => {
+    const produkte = data['produkte'];
+    const kategorie = data['kategorien'];
+    let index = -1;
+    for(let i=0; i< kategorie.length; i++){
+        let result = (kategorie[i].name.localeCompare(kategorieName))
+        if( result === 0) {
+            index = kategorie[i].id;
+        }
+    }
+
+    if(index !== undefined){
+        return produkte.filter(element => element.kategorie === index)
+            .map(element => element.anzahl)
+            .reduce((accumulaltor, currentValue) => accumulaltor + currentValue)
+    }
+    return "Error";
+}
+
+
+const showResult = () => {
+    const suchValue = document.getElementById("function-name").value;
+    const titleDom = document.getElementById("title");
+    titleDom.textContent = suchValue;
+    const result = document.getElementById("result");
+    switch (suchValue) {
+        case "getMaxPreis":
+            result.textContent = getMaxPreis(data);
+            break;
+        case "getMinPreis":
+            result.textContent = getMinPreis(data);
+            break;
+        case "getPreisSum":
+            result.textContent = getPreisSum(data);
+            break;
+        case "getGesamtWert":
+            result.textContent = getGesamtWert(data);
+            break;
+        case "getAnzahlProdukteOfKategorie":
+            result.textContent = getAnzahlProdukteOfKategorie(data, 'Garten');
+            break;
+        default:
+            result.textContent = "Funktion nicht gefunden";
+    }
+}
+
+/*+++++++++++CREATE NAV LIST+++++++++++++*/
+
